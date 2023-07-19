6b5c724235c389a2ab89cb5cb748347d97825a53
jameshuang0430 <yi.11026306@gmail.com>
2023-07-19 15:31:45 +0800

    Finishing p113 exercise in the book.

---
+++ chap3/p113PlacingAClassInASeparateFileForReusability/fig03_10.cpp
@@ -0,0 +1,14 @@
+//include class GradeBook from file GradeBook.h for use in main.
+#include<iostream>
+#include "GradeBook.h" // include definition of class GradeBook
+using namespace std;
+
+int main()
+{
+    GradeBook gradeBook1("CS101 Introduction to C++ Programming");
+    GradeBook gradeBook2("CS102 Data Structures in C++");
+
+    cout << "gradebook1 created for course: " << gradeBook1.getCourseName() << endl;
+    cout << "gradebook2 created for course: " << gradeBook2.getCourseName() << endl;
+
+}
