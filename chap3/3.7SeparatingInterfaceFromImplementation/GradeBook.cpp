9194b6a8722146daaa480f772332f9d95db47ea1
jameshuang0430 <yi.11026306@gmail.com>
2023-08-04 04:05:09 +0800

    Finishing GradeBook.cpp

---
+++ chap3/3.7SeparatingInterfaceFromImplementation/GradeBook.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "GradeBook.h" // include definition of class GradeBook
+using std::cout, std::endl;
+
+// constructor initializes courseName with string supplied as argument 
+GradeBook::GradeBook( string name )
+{
+    setCourseName( name ); // call set function to initialize courseName
+}
+
+// function to set the course name
+void GradeBook::setCourseName( string name )
+{
+    courseName = name; // store the course name in the object 
+}
+
+// function to get the course name 
+string GradeBook::getCourseName()
+{
+    return courseName; // return object's courseName 
+}
+
+//display a welcome message to the GradeBook user
+void GradeBook::displayMessage()
+{
+    // call getCourseName to get the courseName
+    cout << "Welcome to the grade book for\n" << getCourseName()
+        << "!" << endl;
+}
